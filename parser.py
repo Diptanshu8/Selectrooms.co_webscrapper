c0474a5940f41b051b76c850f09a61fd6bc30bd3
Diptanshu8 <diptanshuj@gmail.com>
2016-04-27 00:34:28 +0530

    The parser.py can extract data but it is in \xa**,*** format This needs formatting and this formatting can be very easily done using python map function but search for something in regex to not match \xa

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import requests
+import requests,re
 from bs4 import BeautifulSoup
 
 http_proxy  = "http://10.3.100.207:8080"
@@ -9,7 +9,9 @@ proxyDict = { "http"  : http_proxy, "https" : https_proxy,"ftp"   : ftp_proxy}
 url = 'https://www.tripadvisor.in/MiniMetaCRAjax?detail=190176&inMonth=11%202016&inDay=1&outMonth=11%202016&outDay=2&adults=2&rooms=1&area=QC_Meta_Mini&returnAllOffers=true&imp=true&metaReferer=Hotel_Review&baseLocId=193049&metaRequestTiming=1454318641754&isFirstPageLoad=false&finalRequest=true&isDateChangedRequest=true'
 r = requests.get(url,proxies = proxyDict)
 html = r.content
-soup = BeautifulSoup(html)
-data = soup.findAll('div',{"class":"no_cpu offer wrap premium                   avail                  hacComplete                "})
-
-print data
+soup = BeautifulSoup(html,'lxml')
+data = soup.findAll('div',{"class":"price"}) #"no_cpu offer wrap premium                   avail                  hacComplete                "})
+data = map(str,data)
+#find an appropriate way to do this matching
+final = re.findall('\xa0[0-9]*,[0-9]*'," ".join(data))
+print final
